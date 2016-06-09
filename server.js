d1debe0b16aff79b34cb605f67f10e7f3cb020c1
sandeep lagisetty <lagisetty.s@husky.neu.edu>
2016-06-09 16:22:56 -0400

    test

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static(__dirname + '/public'));
 var assignment = require('./assignment/app.js');
 assignment(app);
 
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
