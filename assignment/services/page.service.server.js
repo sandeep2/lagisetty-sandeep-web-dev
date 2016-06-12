cbdaec5607b505e0573c5d759824a1db922f8054
sandeep lagisetty <lagisetty.s@husky.neu.edu>
2016-06-12 14:12:17 -0400

    remove old data

--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -4,11 +4,6 @@
 module.exports = function(app,models){
 
     var pageModel = models.pageModel;
-    var pages = [
-        { "_id": "321", "name": "Post 1", "websiteId": "456" },
-        { "_id": "432", "name": "Post 2", "websiteId": "456" },
-        { "_id": "543", "name": "Post 3", "websiteId": "456" }
-    ];
 
     app.get("/api/page/:pageId",findPageById);
     app.get("/api/website/:websiteId/page",findAllPages);
