3f4fc60eb7493bab080b1d2b913e7c89bb198905
Sandeep Lagisetty <slagisetty@carboniteinc.com>
2016-06-26 01:31:07 -0400

    fix styling for users pets

--- project/models/pet/pet.schema.server.js
+++ project/models/pet/pet.schema.server.js
@@ -4,13 +4,13 @@
 module.exports = function(){
     var mangoose = require("mongoose");
     var PetSchema = mangoose.Schema({
-        petId: {type: String, required: true, unique: true},
+        petId: {type: String, required: true, index: {unique: true}},
         media:[String],
         description: String,
         sex: String,
         name: String,
         age: String,
-        breeds: String,
+        breed: String,
         contact: String
     },{collection: "project.pet"});
 
--- public/project/views/petProfile/pet-user.controller.client.js
+++ public/project/views/petProfile/pet-user.controller.client.js
@@ -6,9 +6,10 @@
         .module("WhereIsMyPet")
         .controller("PetUserController", PetUserController);
 
-    function PetUserController($routeParams, UserService,$scope) {
+    function PetUserController($routeParams, UserService,$scope,$rootScope) {
         var vm = this;
         var id = $routeParams.userId;
+        
 
         function init() {
                 UserService
@@ -21,6 +22,48 @@
         }
 
         init();
+
+        vm.likePets = function (pet) {
+            if($rootScope.currentUser == null){
+                $('#notLoggedInDialog').modal('show');
+                return;
+            }
+            userId = $rootScope.currentUser._id;
+            var newPet = {};
+            newPet.id = pet.petId;
+            newPet.image = pet.media;
+            newPet.age = pet.age;
+            newPet.breed = pet.breed;
+            newPet.sex = pet.sex;
+            UserService
+                .likePets(userId,newPet)
+                .then(function(user){
+                    console.log(user.favorites);
+                    $rootScope.currentUser.favorites = user.data.favorites;
+                    console.log($rootScope.currentUser.favorites);
+                },function(error){
+                    vm.error = "error in liking pet"
+                })
+        };
+
+        vm.unlikePets = function(pet){
+            userId = $rootScope.currentUser._id;
+            pet.id = pet.petId;
+            UserService
+                .unlikePets(userId,pet)
+                .then(function(user){
+                    $rootScope.currentUser.favorites = user.data.favorites;
+                    init();
+                },function (error) {
+                    vm.error = "Error in unliking pets";
+                });
+
+        };
+
+        $('.sign-in-up-buttons').click(function (e) {
+            $('#notLoggedInDialog').modal('hide');
+        });
+
     }
 
 })();
